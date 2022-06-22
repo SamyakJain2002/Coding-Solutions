e1dd83221a89848bc13e78a902e98839be5ccdcd
Samyak Jain <56781951+SamyakJain2002@users.noreply.github.com>
2022-06-22 17:39:49 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
@@ -0,0 +1,124 @@
+// { Driver Code Starts
+// C++ program to merge k sorted arrays of size n each
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List node
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node)
+{
+	while (node != NULL)
+	{
+		printf("%d ", node->data);
+		node = node->next;
+	}
+	cout<<endl;
+}
+
+ // } Driver Code Ends
+/*Linked list Node structure
+
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/ 
+
+class Solution{
+  public:
+    //Function to merge K sorted linked list.
+    Node * mergeKLists(Node *arr[], int K)
+    {
+        if(K<=1)
+        return arr[0];
+        
+        Node* head1 = arr[0];
+        for(int i=1;i<K;i++){
+            Node* head2 = arr[i];
+            Node* prev = NULL;
+            Node* temp = head1;
+            if(head1->data > head2->data){
+                head1 = head2;
+                head2 = temp;
+                temp = head1;
+            }
+            while(temp != NULL && head2!=NULL){
+                while(temp!=NULL && temp->data <= head2->data){
+                    prev= temp;
+                    temp = temp->next;
+                }
+                prev->next = head2;
+                if(temp==NULL){
+                    break;
+                }
+                while(head2 !=NULL && head2->data<= temp->data){
+                    prev = head2;
+                    head2 = head2->next;
+                }
+                prev->next = temp;
+            }
+        }
+        return head1;
+    }
+};
+
+
+
+// { Driver Code Starts.
+// Driver program to test above functions
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+	   int N;
+	   cin>>N;
+       struct Node *arr[N];
+       for(int j=0;j<N;j++)
+        {
+           int n;
+           cin>>n;
+
+           int x;
+           cin>>x;
+           arr[j]=new Node(x);
+           Node *curr = arr[j];
+           n--;
+
+           for(int i=0;i<n;i++)
+           {
+               cin>>x;
+               Node *temp = new Node(x);
+               curr->next =temp;
+               curr=temp;
+           }
+   		}
+   		Solution obj;
+   		Node *res = obj.mergeKLists(arr,N);
+		printList(res);
+
+   }
+
+	return 0;
+}
+  // } Driver Code Ends
