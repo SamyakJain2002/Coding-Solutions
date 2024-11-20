597259fd36c9128094f68e0b8a607d839e5249bb
Samyak Jain <56781951+SamyakJain2002@users.noreply.github.com>
2024-11-20 15:10:01 +0530

    Time: 2 ms (32.81%), Space: 43.4 MB (50.74%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,24 @@
 class Solution {
-    static void recur(int[] nums, int idx, int n, List<List<Integer>> ans, List<Integer> curr){
-        ans.add(new ArrayList<>(curr));
-
-        for(int i=idx;i<n;i++){
-            if(i!=idx && nums[i]==nums[i-1]){
-                continue;
-            } 
-            curr.add(nums[i]);
-            recur(nums, i+1, n, ans, curr);
-            curr.remove(curr.size()-1);
+    static void recur(int []nums, int n, int idx, List<Integer> select, Set<List<Integer>> sol){
+        if(idx == n){
+            sol.add(new ArrayList<>(select));
+            return;
         }
+        recur(nums, n, idx+1, select, sol);
+        select.add(nums[idx]);
+        recur(nums, n, idx+1, select, sol);
+        select.remove(select.size()-1);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Set<List<Integer>> sol = new HashSet<>();
+        List<Integer> select = new ArrayList<>();
         Arrays.sort(nums);
-        List<Integer> curr = new ArrayList<>();
+        recur(nums, nums.length, 0, select, sol);
         List<List<Integer>> ans = new ArrayList<>();
-        recur(nums, 0, nums.length, ans, curr);
+
+        for(List<Integer> i : sol){
+            ans.add(i);
+        }
         return ans;
     }
 }
