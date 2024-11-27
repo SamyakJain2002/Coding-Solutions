aea40f89059f5bb9b9217f7147043da86225262c
Samyak Jain <56781951+SamyakJain2002@users.noreply.github.com>
2024-11-27 18:36:08 +0530

    Time: 0 ms (100%), Space: 4.2 MB (22.46%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil {
+		return q == nil
+	} else if q == nil {
+		return false
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}
