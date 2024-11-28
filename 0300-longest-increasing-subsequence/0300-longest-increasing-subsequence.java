4b9c5eb3c83206285a71d2b61b36b4407ff91474
Samyak Jain <56781951+SamyakJain2002@users.noreply.github.com>
2024-11-28 16:02:43 +0530

    Time: 8 ms (77.72%), Space: 44.2 MB (56.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,45 +1,19 @@
 class Solution {
-    int getAns(int[] nums, int n, int curr, int prev, int[][] dp) {
-        if (curr == n) {
-            return 0;
-        }
-        if (dp[curr][prev + 1] != -1) {
-            return dp[curr][prev + 1];
-        }
-        int take = 0;
-        int notTake = getAns(nums, n, curr + 1, prev, dp);
-        if (prev == -1 || nums[curr] > nums[prev]) {
-            take = getAns(nums, n, curr + 1, curr, dp) + 1;
-        }
-        dp[curr][prev + 1] = Math.max(take, notTake);
-        return dp[curr][prev + 1];
-    }
-    // public int lengthOfLIS(int[] nums) {
-    // int n = nums.length;
-    // int[][] dp = new int[n][n+1];
-    // for(int row[]:dp){
-    // Arrays.fill(row, -1);
-    // }
-    // return getAns(nums, n, 0, -1, dp);
-    // }
 
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-
-        for (int idx = 0; idx < n; idx++) {
-            for (int prev = 0; prev < idx; prev++) {
-                if (nums[idx] > nums[prev]) {
-                    dp[idx] = Math.max(dp[prev] + 1, dp[idx]);
+        List<Integer> sub = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(sub.size() == 0 || nums[i] > sub.get(sub.size()-1)){
+                sub.add(nums[i]);
+            }else{
+                int low = Collections.binarySearch(sub, nums[i]);
+                if(low<0){
+                    low = -(low+1);
                 }
+                sub.set(low, nums[i]);
             }
         }
-
-        int max = 0;
-        for (int i : dp) {
-            max = Math.max(i, max);
-        }
-        return max;
+        return sub.size();
     }
 }
