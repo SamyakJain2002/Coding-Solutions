46ccfe62a1752479757a11aa147cef6dabcaa605
Samyak Jain <56781951+SamyakJain2002@users.noreply.github.com>
2024-11-30 18:36:19 +0530

    Time: 28 ms (19.8%), Space: 42.9 MB (25.35%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,43 +20,25 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        if(node == null){
+        if (node == null) {
             return null;
         }
         Map<Node, Node> mp = new HashMap<>();
         Queue<Node> q = new LinkedList<>();
-        boolean[] vis = new boolean[101];
         q.add(node);
-        while(!q.isEmpty()){
+        mp.put(node, new Node(node.val));
+        while (!q.isEmpty()) {
             Node top = q.poll();
-            if(vis[top.val]){
-                continue;
-            }
-            Node cp = new Node(top.val);
-            mp.put(top, cp);
-            for(Node i: top.neighbors){
-                if(!vis[i.val]){
-                    q.add(i);
-                }
-            }
-            vis[top.val] = true;
-        }
-        
-        Arrays.fill(vis, false);
-        q.add(node);
-        while(!q.isEmpty()){
-            Node top = q.poll();
-            if(vis[top.val]){
-                continue;
-            }
             Node cp = mp.get(top);
-            for(Node i: top.neighbors){
-                if(!vis[i.val]){
+            for (Node i : top.neighbors) {
+                if (mp.containsKey(i)) {
+                    cp.neighbors.add(mp.get(i));
+                } else {
+                    mp.put(i, new Node(i.val));
                     q.add(i);
+                    cp.neighbors.add(mp.get(i));
                 }
-                cp.neighbors.add(mp.get(i));
             }
-            vis[top.val] = true;
         }
         return mp.get(node);
     }
